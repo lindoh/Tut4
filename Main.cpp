bd5954f395308cd241d6d015ab701487a5f33a87
lindoh <lindohgamede@gmail.com>
2015-04-19 17:12:57 +0200

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Stack.h"
+#include "stdlib.h"
+
+using namespace std;
+
+int main()
+{
+	Stack x;
+
+	int a;
+
+	cout << "Enter a value" << endl;
+	cin >> a;
+
+
+	x.push();
+	x.peek();
+
+	system("pause");
+
+	return 0;
+}
+
